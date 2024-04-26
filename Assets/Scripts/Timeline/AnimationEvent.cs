39045ab4ea59408471914d3c3a54675ca6e311dc
Jack Myrick <contact@jackmyrick.design>
2024-04-26 19:42:47 +0100

    Added animation Event script

---
+++ Assets/Scripts/Timeline/AnimationEvent.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "New Animation Event", menuName = "Animation Event")]
+public class AnimationEvent : ScriptableObject
+{
+    public float timeInSeconds;
+    public UnityEvent onTrigger;
+}
