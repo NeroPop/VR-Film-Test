c416246b51d383f51fd9a9a627a2e07e68be973a
Jack Myrick <contact@jackmyrick.design>
2024-04-27 22:18:08 +0100

    commented in the Event Manager script to make it easier to understand

--- Assets/Scripts/Timeline/AnimationEventManager.cs
+++ Assets/Scripts/Timeline/AnimationEventManager.cs
@@ -31,6 +31,7 @@ public class AnimationEventManager : MonoBehaviour
 
     private void Start()
     {
+        //Sets the time to where you want to start and tells you where it started in debug
         CurrentTime = StartTime;
         Debug.Log("Manager Started");
 
@@ -47,16 +48,18 @@ public class AnimationEventManager : MonoBehaviour
             // Invoke the OnUpdate event with the current time
             OnUpdate?.Invoke(Time.deltaTime);
 
+            //Sets the current time when not paused.
             CurrentTime = CurrentTime + Time.deltaTime;
 
             if (DebugTime)
             {
-                //debug.logging current time
+                //debug.logging current time if you check the option in the inspector.
                 Debug.Log(CurrentTime);
             }
 
             // List<GameObject> eventsToRemove = new List<GameObject>(); // Collect events to remove
-
+            
+            //Checks the animation events in the list and if the current time is the same or greater than the time set in the event then it triggers them.
             foreach (var animEventObject in animationEvents)
             {
                 AnimationEventTrigger animEvent = animEventObject.GetComponent<AnimationEventTrigger>();
@@ -68,6 +71,7 @@ public class AnimationEventManager : MonoBehaviour
                 }
             }
 
+            // If Pause has been turned off then say in console + invoke the toggle pause event + set's triggered pause to false so it only triggers once.
             if(TriggeredPause)
             {
                 Debug.Log("Resumed");
@@ -82,6 +86,7 @@ public class AnimationEventManager : MonoBehaviour
             } */
         }
 
+        //If paused, it will say so in the console + invoke the toggle paused event and set Triggered pause to true so it only triggers once.
         else
         {
             if (!TriggeredPause)
