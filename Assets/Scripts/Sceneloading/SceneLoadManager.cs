7a8eaf1dafe768e55a3e5538a6479aeb17535bd4
Jack Myrick <contact@jackmyrick.design>
2024-04-28 00:30:43 +0100

    Updated Scene Manager so it no longer goes past the number of scenes added in the build

--- Assets/Scripts/Sceneloading/SceneLoadManager.cs
+++ Assets/Scripts/Sceneloading/SceneLoadManager.cs
@@ -6,16 +6,20 @@ using UnityEngine.SceneManagement;
 public class SceneLoadManager : MonoBehaviour
 {
     [SerializeField]
-    int PrevLevel = 0;
+    int CurLevel = 1;
 
     [SerializeField]
-    int CurLevel = 1;
+    int PrevLevel = 0;
 
     [SerializeField]
     int NextLevel = 2;
 
+    [SerializeField]
+    int MaxLevel;
+
     private void Start()
     {
+        //Loads the current selected scene additvely on start and sets next/prev fields
         if (CurLevel > 0)
         {
             SceneManager.LoadScene(CurLevel, LoadSceneMode.Additive);
@@ -24,6 +28,7 @@ public class SceneLoadManager : MonoBehaviour
             NextLevel = CurLevel + 1;
         }
 
+        //If the current scene is set to master then it will automatically start scene 1 and set the levels
         else
         {
             CurLevel = 1;
@@ -32,30 +37,41 @@ public class SceneLoadManager : MonoBehaviour
 
             SceneManager.LoadScene(CurLevel, LoadSceneMode.Additive);
         }
+        //Finds the number of scenes so we don't exceed it.
+        MaxLevel = SceneManager.sceneCountInBuildSettings;
     }
 
     public void NextScene()
     {
-        
-        SceneManager.LoadScene(NextLevel, LoadSceneMode.Additive);
-
-        if(CurLevel > 0)
+        //Checks that there is a next scene to go to
+        if(NextLevel < MaxLevel)
         {
-            SceneManager.UnloadSceneAsync(CurLevel);
-        }
+            // Loads the next scene
+            SceneManager.LoadScene(NextLevel, LoadSceneMode.Additive);
 
-        NextLevel += 1;
-        PrevLevel += 1;
-        CurLevel += 1;
+            //Deletes the current scene if its not master
+            if (CurLevel > 0)
+            {
+                SceneManager.UnloadSceneAsync(CurLevel);
+            }
+
+            //Updates the current, next and previous scene values.
+            NextLevel += 1;
+            PrevLevel += 1;
+            CurLevel += 1;
+        }
     }
 
     public void PrevScene()
     {
+        //Checks that the previous level isn't master
         if (PrevLevel > 0)
         {
+            //Loads the previous scene and Unloads the current scene
             SceneManager.LoadScene(PrevLevel, LoadSceneMode.Additive);
             SceneManager.UnloadSceneAsync(CurLevel);
 
+            //Updates the current, next and previous scene values.
             NextLevel -= 1;
             PrevLevel -= 1;
             CurLevel -= 1;
