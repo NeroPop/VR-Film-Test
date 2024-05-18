2102841b731c7b158f978f916462c4fd43d4a4ac
Jack Myrick <contact@jackmyrick.design>
2024-05-18 01:30:40 +0100

    removed random using xr in music manager script

--- Assets/Scripts/Music & Sounds/MusicManager.cs
+++ Assets/Scripts/Music & Sounds/MusicManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Random = UnityEngine.Random;
-using UnityEngine.XR.Interaction.Toolkit.AffordanceSystem.Receiver.Primitives;
 
 [RequireComponent(typeof(AudioSource))]
 
