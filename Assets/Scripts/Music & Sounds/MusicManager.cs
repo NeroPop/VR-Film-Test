cad06df87aa3b48bde5f00abc74919a07ab6dce4
Jack Myrick <contact@jackmyrick.design>
2024-05-22 23:21:28 +0100

    tidied up the script

--- Assets/Scripts/Music & Sounds/MusicManager.cs
+++ Assets/Scripts/Music & Sounds/MusicManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Random = UnityEngine.Random;
-using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 
@@ -16,67 +15,12 @@ public class MusicManager : MonoBehaviour
     [SerializeField]
     private AudioClip[] Tracks;
 
-    //Audio to be used in the playlist
-  /*  [SerializeField]
-    AudioClip Track1;
-    [SerializeField]
-    AudioClip Track2;
-    [SerializeField]
-    AudioClip Track3;
-    [SerializeField]
-    AudioClip Track4;
-    [SerializeField]
-    AudioClip Track5;
-    [SerializeField]
-    AudioClip Track6;
-    [SerializeField]
-    AudioClip Track7;
-    [SerializeField]
-    AudioClip Track8;
-    [SerializeField]
-    AudioClip Track9;
-    [SerializeField]
-    AudioClip Track10;
-    [SerializeField]
-    AudioClip Track11;
-    [SerializeField]
-    AudioClip Track12;
-    [SerializeField]
-    AudioClip Track13;
-    [SerializeField]
-    AudioClip Track14;
-    [SerializeField]
-    AudioClip Track15;
-    [SerializeField]
-    AudioClip Track16;
-    [SerializeField]
-    AudioClip Track17;
-    [SerializeField]
-    AudioClip Track18;
-    [SerializeField]
-    AudioClip Track19;
-    [SerializeField]
-    AudioClip Track20;
-    [SerializeField]
-    AudioClip Track21;
-    [SerializeField]
-    AudioClip Track22;
-    [SerializeField]
-    AudioClip Track23;
-    [SerializeField]
-    AudioClip Track24;
-    [SerializeField]
-    AudioClip Track25;
-    [SerializeField]
-    AudioClip Track26; */
-
     [Header("Display Elements")]
 
     //Texts to be displayed in UI
     public TMP_Text DisplayName;
     public TMP_Text DisplayTime;
     public TMP_Text DisplayRemaining;
-    public Slider DisplayProgress;
     public Slider audioSlider;
     public GameObject PauseButton;
     public GameObject ResumeButton;
@@ -117,7 +61,6 @@ public class MusicManager : MonoBehaviour
     private float remaining;
     private float remainingseconds;
     private float remainingminutes;
-    private bool dragged = false;
 
 
     //Tells unity what onUnpdate is because it's dumb
@@ -143,59 +86,7 @@ public class MusicManager : MonoBehaviour
     }
 
     private void Update()
-    { /*
-        //Checks if paused
-        if (!Paused)
-        {
-            //Keeps track of time so we know how far we are into the song
-            OnUpdate?.Invoke(Time.deltaTime);
-            CurrentTime = CurrentTime + Time.deltaTime;
-            progress = CurrentTime / TrackLength * 100;
-
-            //Calculates the time we have remaining
-            remaining = TrackLength - CurrentTime;
-            remainingminutes = ((int)remaining) / 60;
-            remainingseconds = remaining - (remainingminutes * 60);
-
-            //Displays the current progress
-            DisplayProgress.value = progress;
-
-            if (!isDragging)
-            {
-                audioSlider.value = CurrentTime;
-            }
-
-            //Calculates minutes and seconds for time taken
-            if (displayseconds <= 59.5)
-            {
-                displayseconds = displayseconds + Time.deltaTime;
-            }
-            else if (displayseconds >= 59.5)
-            {
-                displayseconds = 0;
-                displayminutes += 1;
-            }
-
-            //Displays the time in minutes and seconds
-            if (displayseconds < 9.5)
-            {
-                DisplayTime.text = displayminutes.ToString("F0") + " : 0" + displayseconds.ToString("F0");
-            }
-            else if (displayseconds > 9.5)
-            {
-                DisplayTime.text = displayminutes.ToString("F0") + " : " + displayseconds.ToString("F0");
-            }
-
-            //Displays the time remaining in minutes and seconds
-            if (remainingseconds < 9.5)
-            {
-                DisplayRemaining.text = "-" + remainingminutes.ToString("F0") + " : 0" + remainingseconds.ToString("F0");
-            }
-            else if (remainingseconds > 9.5)
-            {
-                DisplayRemaining.text = "-" + remainingminutes.ToString("F0") + " : " + remainingseconds.ToString("F0");
-            }
-        }*/
+    { 
         AudioSource audio = GetComponent<AudioSource>();
         TrackLength = audio.clip.length;
 
@@ -205,7 +96,7 @@ public class MusicManager : MonoBehaviour
             CurrentTime += Time.deltaTime;
 
             float progress = CurrentTime / TrackLength;
-            DisplayProgress.value = progress;
+            //DisplayProgress.value = progress;
 
             if (!isDragging)
             {
@@ -224,18 +115,6 @@ public class MusicManager : MonoBehaviour
         audio.Play();
          
         Debug.Log("Playing Track " + TrackNumber + " Song Length " + audio.clip.length.ToString("F2") + " Playing from " + CurrentTime.ToString("F2"));
-
-       /* //Displays the song name
-        DisplayName.text = audio.clip.name;
-
-        // Set the slider's max value to the length of the audio clip
-        audioSlider.maxValue = audio.clip.length;
-
-        // Add listeners to handle the slider events
-        audioSlider.onValueChanged.AddListener(OnSliderValueChanged);
-
-        //Figures out how long is left of the audio clip and then waits until it's finished before continuing
-        ClipTime = audio.clip.length - CurrentTime; */
 
         yield return new WaitForSeconds(audio.clip.length - CurrentTime);
 
@@ -312,8 +191,6 @@ public class MusicManager : MonoBehaviour
         //If shuffled, it calls the shuffle function, if not shuffled it increases the track number and then plays the next song
         if (!Shuffle)
         {
-            /* PrevTrack = TrackNumber;
-             TrackNumber += 1; */
             TrackNumber = (TrackNumber + 1) % Tracks.Length;
             PlaySong();
         }
@@ -326,22 +203,6 @@ public class MusicManager : MonoBehaviour
 
     public void Back()
     {
-        //Plays the previous song
-        //If shuffled it plays the previous track number, if not shuffled it simply goes back 1 track
-        /* if (!Shuffle)
-         {
-             StopCoroutine(Playing());
-             TrackNumber -= 1;
-             PlaySong();
-         }
-
-         else if (Shuffle)
-         {
-             StopCoroutine(Playing());
-             TrackNumber = PrevTrack;
-             PlaySong();
-         }*/
-
         StopCoroutine(Playing());
         if (Shuffle)
         {
@@ -358,14 +219,12 @@ public class MusicManager : MonoBehaviour
     {
         isDragging = true;
         Pause();
-        dragged = false;
     }
 
     public void OnPointerUp()
     {
         isDragging = false;
         Resume();
-        dragged = true;
     }
 
     public void Pause()
@@ -399,21 +258,6 @@ public class MusicManager : MonoBehaviour
 
     private void Shuffled()
     {
-        //Saves the current track as the previous track before randomising the current track
-        /*   PrevTrack = TrackNumber;
-           TrackNumber = Random.Range(1, 27);
-
-           //Checks to make sure you can't hear the same song twice in a row
-           if (TrackNumber == PrevTrack)
-           {
-               Shuffled();
-           }
-
-           else if (TrackNumber != PrevTrack)
-           {
-               PlaySong();
-           } */
-
         PrevTrack = TrackNumber;
         do
         {
@@ -449,11 +293,8 @@ public class MusicManager : MonoBehaviour
             CurrentTime = value;
 
             displayseconds = CurrentTime;
-
             displayminutes = ((int)displayseconds) / 60;
             displayseconds = displayseconds - (displayminutes * 60);
-
-            dragged = true;
         }
     }
 
