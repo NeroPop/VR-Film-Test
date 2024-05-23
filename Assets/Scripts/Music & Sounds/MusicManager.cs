e7c87902e4eb812187731a8f7d9458b74f934f20
Jack Myrick <contact@jackmyrick.design>
2024-05-23 01:07:28 +0100

    Made the script look v slightly neater and removed the unused script

--- Assets/Scripts/Music & Sounds/MusicManager.cs
+++ Assets/Scripts/Music & Sounds/MusicManager.cs
@@ -12,16 +12,17 @@ public class MusicManager : MonoBehaviour
 {
     [Header("Soundtrack Audio")]
 
+    //List of all the audio tracks
     [SerializeField]
     private AudioClip[] Tracks;
 
     [Header("Display Elements")]
 
-    //Texts to be displayed in UI
+    //UI elements
     public TMP_Text DisplayName;
     public TMP_Text DisplayTime;
     public TMP_Text DisplayRemaining;
-    public Slider audioSlider;
+    public Slider AudioSlider;
     public GameObject PauseButton;
     public GameObject ResumeButton;
 
@@ -52,12 +53,11 @@ public class MusicManager : MonoBehaviour
     [SerializeField]
     private bool isDragging = false;
 
-    //Only used in the script
+    //Only used in the script internally
     private float ClipTime = 0;
     private int PrevTrack;
     private float displayseconds;
     private float displayminutes;
-    private float progress;
     private float remaining;
     private float remainingseconds;
     private float remainingminutes;
@@ -82,65 +82,73 @@ public class MusicManager : MonoBehaviour
         }
 
         // Add listeners to handle the slider events
-        audioSlider.onValueChanged.AddListener(OnSliderValueChanged);
+        AudioSlider.onValueChanged.AddListener(OnSliderValueChanged);
     }
 
     private void Update()
-    { 
+    {
+        //Get's a reference for the AudioSource
         AudioSource audio = GetComponent<AudioSource>();
+
+        //Figures out the track length of the audio clip
         TrackLength = audio.clip.length;
 
+        //checks if the music is paused
         if (!Paused)
         {
+            //Adds 1 second to current time every second
             OnUpdate?.Invoke(Time.deltaTime);
             CurrentTime += Time.deltaTime;
 
-            float progress = CurrentTime / TrackLength;
-            //DisplayProgress.value = progress;
-
+            //If the user isnt dragging the audio slider then it sets the audio slider to the current time to keep track of progress
             if (!isDragging)
             {
-                audioSlider.value = CurrentTime;
+                AudioSlider.value = CurrentTime;
             }
         }
+        //Displays the current and remaining times
         DisplayRemaining.text = "-" + FormatTime(TrackLength - CurrentTime);
         DisplayTime.text = FormatTime(CurrentTime);
     }
 
     IEnumerator Playing()
     {
-        //Get's a reference for the AudioSource and displays in the console what song is playing, how long it is and where we are starting from
+        //Get's a reference for the AudioSource
         AudioSource audio = GetComponent<AudioSource>();
 
+        //Starts playing the audio clip
         audio.Play();
          
+        //Sends a message to the console about what track is playing, how long it is and what time it's playing from.
         Debug.Log("Playing Track " + TrackNumber + " Song Length " + audio.clip.length.ToString("F2") + " Playing from " + CurrentTime.ToString("F2"));
 
+        //Waits until the end of the song
         yield return new WaitForSeconds(audio.clip.length - CurrentTime);
 
          //Checks if the music is paused
          if (!Paused)
          {
-            //If the music isn't shuffled it saves the current track (incase it gets put on shuffle) and Increases the track number before beggining the next track
-            //Also checks that the clip has definitely finished playing before starting the next song
+            //Checks if the music isn't shuffled
             if (!Shuffle)
              {
+                //Checks that the clip has definitely finished
                  if (CurrentTime >= audio.clip.length)
                  {
+                    //Sets the previous track as the current track before setting the current track to the next one and calling the PlaySong function
                      PrevTrack = TrackNumber;
-                    // TrackNumber += 1;
                      TrackNumber = (TrackNumber + 1) % Tracks.Length;
                      PlaySong();
                      StopCoroutine(Playing());
                  }
              }
 
-             //If the music is on shuffle, it calls the Shuffled function which saves the current Track as the previous one and plays a random track next
-             //Also checks that the clip has definitely finished playing before starting the next song
+             //Checks if the music is on shuffle
              else if (Shuffle)
              {
-                 if (CurrentTime >= audio.clip.length)
+                //Checks that the clip has definitely finished
+                if (CurrentTime >= audio.clip.length)
                  {
+                    //Calls tbe shuffled function
                      Shuffled();
                      StopCoroutine(Playing());
                  }
@@ -153,15 +161,15 @@ public class MusicManager : MonoBehaviour
         //Get's a reference for the AudioSource
         AudioSource audio = GetComponent<AudioSource>();
 
+        //Sets the track number to the last track if the current track number is somehow below 0
         if (TrackNumber < 0 || TrackNumber >= Tracks.Length) return;
 
+        //Sets the audio clip as whatever the current track number is
         audio.clip = Tracks[TrackNumber];
+
+        //Sets the times back to 0
         CurrentTime = 0;
-
-        //Sets the audio slider back to 0
-        audioSlider.value = 0;
-
-        //resets the displayed time
+        AudioSlider.value = 0;
         displayseconds = 0;
         displayminutes = 0;
 
@@ -169,14 +177,15 @@ public class MusicManager : MonoBehaviour
         DisplayName.text = audio.clip.name;
 
         // Set the slider's max value to the length of the audio clip
-        audioSlider.maxValue = audio.clip.length;
+        AudioSlider.maxValue = audio.clip.length;
 
         // Add listeners to handle the slider events
-        audioSlider.onValueChanged.AddListener(OnSliderValueChanged);
+        AudioSlider.onValueChanged.AddListener(OnSliderValueChanged);
 
-        //Figures out how long is left of the audio clip and then waits until it's finished before continuing
+        //Figures out how long is left of the audio clip
         ClipTime = audio.clip.length - CurrentTime;
 
+        //Starts the playing couroutine if it's not paused
         if (!Paused)
         {
             StartCoroutine(Playing());
@@ -203,6 +212,8 @@ public class MusicManager : MonoBehaviour
 
     public void Back()
     {
+        //Plays the previous song
+        //If shuffled, it plays the previous track, if not shuffled it takes the current track and removes 1
         StopCoroutine(Playing());
         if (Shuffle)
         {
@@ -217,15 +228,17 @@ public class MusicManager : MonoBehaviour
 
     public void OnPointerDown()
     {
+        //If the user starts holding the mouse pointer down on the slider, it sets whole numbers to true for precision and also dragging to true before calling pause
         isDragging = true;
-        audioSlider.wholeNumbers = true;
+        AudioSlider.wholeNumbers = true;
         Pause();
     }
 
     public void OnPointerUp()
     {
+        //If the user stops holding the mouse pointer down on the slider, it sets whole numbers to false for smooth progress and dragging to false before calling resume
         isDragging = false;
-        audioSlider.wholeNumbers = false;
+        AudioSlider.wholeNumbers = false;
         Resume();
     }
 
@@ -240,6 +253,7 @@ public class MusicManager : MonoBehaviour
         audio.Pause();
         Debug.Log("Paused at " + CurrentTime.ToString("F2"));
 
+        //Switches the pause & resume buttons for UX
         PauseButton.SetActive(false);
         ResumeButton.SetActive(true);
     }
@@ -254,18 +268,22 @@ public class MusicManager : MonoBehaviour
         Paused = false;
         audio.UnPause();
 
+        //Switches the pause & resume buttons for UX
         PauseButton.SetActive(true);
         ResumeButton.SetActive(false);
     }
 
     private void Shuffled()
     {
+        //Sets the previous track to the current track number
         PrevTrack = TrackNumber;
         do
         {
+            //randomises the next track from the tracks list
             TrackNumber = Random.Range(0, Tracks.Length);
         } while (TrackNumber == PrevTrack);
 
+        //plays the random song
         PlaySong();
     }
 
@@ -294,6 +312,7 @@ public class MusicManager : MonoBehaviour
             audio.time = value;
             CurrentTime = value;
 
+            //update the display times to match the slider value
             displayseconds = CurrentTime;
             displayminutes = ((int)displayseconds) / 60;
             displayseconds = displayseconds - (displayminutes * 60);
@@ -302,6 +321,7 @@ public class MusicManager : MonoBehaviour
 
     private string FormatTime(float time)
     {
+        //formats time into seconds and minutes to be displayed properly
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time % 60);
         return $"{minutes:00}:{seconds:00}";
